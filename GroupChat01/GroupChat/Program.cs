e0bbf38afb7ad10c0428d622f068c4e9f4fac966
Garrett Girod <girodg@hotmail.com>
2022-11-28 21:01:57 -0500

    group chat 01

---
+++ GroupChat01/GroupChat/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GroupChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1) return a true or false
+            //2) using the out parameter, return the # if the string can be converted to an int
+            string usersInput = "5";
+            if(int.TryParse(usersInput, out int theNumber) == false)
+                Console.WriteLine($"{usersInput} is NOT a number." );
+            else //isANumber is true
+                Console.WriteLine($"{usersInput} is a number: {theNumber}.");
+        }
+    }
+}
