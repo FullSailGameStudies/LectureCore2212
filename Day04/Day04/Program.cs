85bf5950d8a2741be73bbab4563d9ca6cb0b0f0b
Garrett Girod <girodg@hotmail.com>
2022-12-02 09:47:24 -0500

    Write CSV

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Day04
@@ -8,6 +9,23 @@ namespace Day04
     {
         static void Main(string[] args)
         {
+            string filePath = @"C:\temp\2212";
+            string fileName = "sample.txt";
+            string fullPath = Path.Combine(filePath, fileName);
+
+            char delimiter = '#';
+            //1) Open the file. will create/overwrite
+            using (StreamWriter sw = new StreamWriter(fullPath))
+            {
+                //2) write to the file
+                sw.Write("Batman rules! Aquaman does not.");
+                sw.Write(delimiter);
+                sw.Write(5);
+                sw.Write(delimiter);
+                sw.Write(13.7);
+                sw.Write(delimiter);
+                sw.Write(true);
+            }//3) CLOSE THE FILE!
         }
     }
 }
