f8612a7fb38cfdc89ea1bc6e45ca2516496d5640
Garrett Girod <girodg@hotmail.com>
2022-12-02 10:17:10 -0500

    added day4 material

---
+++ Day04/Day04/Course.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04
+{
+    public class Course
+    {
+        private Dictionary<string, double> _grades;
+
+        public string Name { get; set; } = String.Empty;
+
+        public Course()
+        {
+            FillGrades();
+        }
+
+
+
+        #region Day03 Code
+        public void FillGrades()
+        {
+            List<string> students = new List<string>()
+            {
+                "Anthony", "Julio", "Jeffrey", "ID", "Jonah", "Jake", "Jesus", "Sam",
+                "Albert", "Hanna", "Connor", "Aidan", "Carter", "Alexis", "Kemarly",
+                "Joaquin", "Enrique"
+            };
+            Random rando = new Random();
+            _grades = new Dictionary<string, double>();
+            _grades.Add("Steev", rando.NextDouble() * 100);
+            _grades.Add("Anita", rando.NextDouble() * 100);
+            _grades.Add("Cameron", rando.NextDouble() * 100);
+            _grades.Add("dylan", rando.NextDouble() * 100);
+            _grades.Add("Frankie", rando.NextDouble() * 100);
+            _grades.Add("Japhet", rando.NextDouble() * 100);
+            foreach (var student in students)
+            {
+                _grades[student] = rando.NextDouble() * 100;
+            }
+        }
+        public void PrintGrades()
+        {
+            if (_grades == null) FillGrades();
+            Console.WriteLine($"---------{Name}----------");
+            foreach (var student in _grades)
+            {
+                string name = student.Key;
+                double grade = student.Value;
+                Console.ForegroundColor = (grade < 59.5) ? ConsoleColor.Red :
+                                          (grade < 69.5) ? ConsoleColor.DarkYellow :
+                                          (grade < 79.5) ? ConsoleColor.Yellow :
+                                          (grade < 89.5) ? ConsoleColor.Blue :
+                                          ConsoleColor.Green;
+                Console.Write($"{grade,7:N2} ");
+                Console.ResetColor();
+                Console.WriteLine(name);
+            }
+        }
+        public void DropStudent()
+        {
+            if (_grades == null) FillGrades();
+            do
+            {
+                PrintGrades();
+                Console.Write("Student to drop? ");
+                string student = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(student)) break;
+
+                bool wasDropped = _grades.Remove(student);
+                if (wasDropped)
+                {
+                    PrintGrades();
+                    Console.WriteLine($"{student} was dropped from {Name}.");
+                }
+                else
+                    Console.WriteLine($"{student} was not in {Name}.");
+            } while (true);
+        }
+        public void CurveStudent()
+        {
+            if (_grades == null) FillGrades();
+            do
+            {
+                PrintGrades();
+                Console.Write("Student to curve? ");
+                string student = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(student)) break;
+
+                if (_grades.TryGetValue(student, out double grade))
+                {
+                    _grades[student] = Math.Min(100, grade + 5);
+                    PrintGrades();
+                    Console.WriteLine($"{student} was curved from {grade} to {_grades[student]}.");
+                }
+                else
+                    Console.WriteLine($"{student} was not in {Name}.");
+            } while (true);
+        }
+        #endregion
+    }
+}
--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -14,8 +14,9 @@ namespace Day04
             string fullPath = Path.Combine(filePath, fileName);
 
             char delimiter = '#';
+            #region Write CSV
             //1) Open the file. will create/overwrite
-            using (StreamWriter sw = new StreamWriter(fullPath))
+            using (StreamWriter sw = new StreamWriter(fileName))
             {
                 //2) write to the file
                 sw.Write("Batman rules! Aquaman does not.");
@@ -25,7 +26,89 @@ namespace Day04
                 sw.Write(13.7);
                 sw.Write(delimiter);
                 sw.Write(true);
-            }//3) CLOSE THE FILE!
+            }//3) CLOSE THE FILE! 
+            #endregion
+
+            #region Read CSV
+
+            if (File.Exists(fullPath))
+            {
+                string fileText = File.ReadAllText(fullPath);//open, read, close
+                Console.WriteLine(fileText);
+                string[] data = fileText.Split(delimiter);
+
+                foreach(string dataItem in data)
+                    Console.WriteLine(dataItem);
+            }
+
+            #endregion
+
+
+            Course pg2 = new Course() { Name = "PG2 - 2212" };
+
+            string[] menu = new string[]
+            {"0. Exit", "1. Part 1 - Saving CSV", "2. Part 2 - Reading CSV", "3. Part 3 - Serializing JSON", "4. Part 4 - Deserializing JSON","5. Part 5 - Recursive Loop"};
+
+            int part = 1;
+            while (true)
+            {
+                Console.Clear();
+
+                #region Show Menu
+                Console.WriteLine("What part to run?");
+                foreach (var option in menu)
+                    Console.WriteLine(option);
+                Console.Write("Choice? ");
+                string input = Console.ReadLine();
+                #endregion
+
+                if (int.TryParse(input, out part) && part > 0 && part < 6)
+                {
+                    switch (part)
+                    {
+                        case 1:
+                            //Add a method SaveGrades to the Course class
+                            //  save the dictionary to a csv file
+                            //call SaveGrades here 
+                            break;
+                        case 2:
+                            //Add a method LoadGrades to the Course class
+                            //  clear the _grades dictionary
+                            //  load the dictionary data from the csv file
+                            //call LoadGrades here
+                            break;
+                        case 3:
+                            //Add a SerializeGrades method to the Course class
+                            //  serialize the dictionary to a json file
+                            //call SerializeGrades here 
+                            break;
+                        case 4:
+                            //Add a DeserializeGrades method to the Course class
+                            //  clear the _grades dictionary
+                            //  deserialize the json file to the dictionary
+                            //call DeserializeGrades here 
+                            break;
+                        case 5:
+                            //Add a static Bats method to this file
+                            //  convert this for loop to a recursive loop using the Bats method
+                            /*
+                                for(int i = 0;i < 100;i++)
+                                {
+                                    Console.Write((char)78);
+                                    Console.Write((char)65);
+                                    Console.Write(' ');
+                                }
+                             */
+                            //call Bats here 
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                    break;
+                Console.ReadKey();
+            }
         }
     }
 }
