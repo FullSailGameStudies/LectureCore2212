834ec07361fb158360dccb943027e6389b0840c2
Garrett Girod <girodg@hotmail.com>
2022-12-09 10:08:30 -0500

    Pistol class

---
+++ Day07/Day07CL/Pistol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public class Pistol : Weapon
+    {
+        public Pistol(int magCap, int rounds, int range, int damage) : base(range,damage)
+        {
+            MagCapacity = magCap;
+            Rounds = rounds;
+        }
+
+        public int MagCapacity { get; }
+        public int Rounds { get; }
+    }
+}
--- Day07/Day07CL/Weapon.cs
+++ Day07/Day07CL/Weapon.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day07
+namespace Day07CL
 {
     //
     // edge, club, pistols, rifles, grenades
-    // create a Pistol weapon that derives from Weapon.
+    // create a Pistol weapon class that derives from Weapon.
     //      Data: mag capacity, # of rounds
     //      Methods: Shoot
-    
+
     public abstract class Weapon
     {
         public Weapon(int range, int damage)
