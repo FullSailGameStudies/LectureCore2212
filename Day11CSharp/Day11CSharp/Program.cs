77745ae4d676676e377995ff6933d2920fdf3e14
Garrett Girod <girodg@hotmail.com>
2022-12-14 10:18:10 -0500

    C++ intro

--- Day11CSharp/Day11CSharp/Program.cs
+++ Day11CSharp/Day11CSharp/Program.cs
@@ -7,6 +7,7 @@ namespace Day11CSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine($"{sizeof(char)}");
         }
     }
 }
